d7e4b7dda0f22f614927a4a1dd2d42707e2faa5d
Baskoro Nugroho <bas.nugroho@yahoo.com>
2018-10-22 00:21:47 +0700

    add classify_image.py and some func

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, requests
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 import bcrypt
@@ -21,7 +21,7 @@ def UserExist(username):
 
 class Register(Resource):
     def post(self):
-        postedData = requests.get_json()
+        postedData = request.get_json()
 
         username = postedData['username']
         password = postedData['password']
@@ -33,7 +33,7 @@ class Register(Resource):
             }
             return jsonify(retJson)
 
-        hashed_pw = bcrypt.hashed_pw(password.encode("utf8"), bcrypt.gensalt())
+        hashed_pw = bcrypt.hashpw(password.encode("utf8"), bcrypt.gensalt())
 
         users.insert({
             "Username": username,
@@ -46,3 +46,67 @@ class Register(Resource):
             "msg": "you succesfully sign up via this API"
         }
         return jsonify(retJson)
+
+def verify_pw(username, password):
+    if not UserExist(username):
+        return False
+    hashed_pw = users.find({
+        "Username": username
+    })[0]['Password']
+
+    if bcrypt.hashpw(password.encode("utf8"), hashed_pw) == hashed_pw:
+        return True
+    else:
+        return False
+
+def generateReturnDictionary(status, message):
+    retJson = {
+        "status": status,
+        "msg": message
+    }
+    return jsonify(retJson)
+
+def verifyCredentials(username, password):
+    if not UserExist(username):
+        return generateReturnDictionary(301, "Invalid Username"), True
+    correct_pw = verify_pw(username, password)
+    if not correct_pw:
+        return generateReturnDictionary(302, "Invalid Password"), True
+    return None, False
+
+class Classify(Resource):
+    def post(self):
+        postedData = request.get_json()
+        username = postedData['username']
+        password = postedData['password']
+        url = postedData['url']
+        # todo
+        retJson, error = verifyCredentials(username, password)
+        if error:
+            return jsonify(retJson)
+        
+        tokens = users.find({
+            "Username": username
+        })[0]["Tokens"]
+        # todo
+        if tokens <= 0:
+            return jsonify(generateReturnDictionary(303, "Not enough tokens"))
+
+        r = requests.get(url)
+        retJson = {}
+        with open("temp.jpg", "wb") as f:
+            f.write(r.content)
+            proc = subprocess.Popen('python_classify_image.py --model_dir=. --image_file=./temp.jpg')
+            proc.communicate()[0]
+            proc.wait()
+            with open('text.txt') as g:
+                retJson = json.load(g)
+        users.update({
+            "Username": username
+        }, {
+            "$set":{
+                "Tokens": tokens - 1
+            }
+        })
+        return retJson
+        
